235010cb649ec3ddad5ec88359c41d3a6b08802b
m.vorobyev <max9680@mail.ru>
2022-06-09 10:51:26 +0300

    Add Cli.php with function

---
+++ src/Cli.php
@@ -0,0 +1,16 @@
+<?php
+namespace BrainGames\Cli;
+
+function line($text, $var1 = null)
+{
+	if ($var1 != null) {
+	    $text = substr_replace($text, $var1, strpos($text,"%s"), 2);
+	}
+	echo ("$text\n");
+}
+
+function prompt($text)
+{
+	echo "$text\n";
+	return trim(fgets(STDIN));
+}

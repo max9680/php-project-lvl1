94c23b576e2af0cf5c6d2f4eae8fcf85f9c4807b
m.vorobyev <max9680@mail.ru>
2022-06-15 12:42:12 +0300

    Modify src/Cli.php

--- src/Cli.php
+++ src/Cli.php
@@ -7,11 +7,11 @@ function line($text, $var1 = null)
     if ($var1 != null) {
         $text = substr_replace($text, $var1, strpos($text, "%s"), 2);
     }
-        echo ("$text\n");
+        echo ("$text");
 }
 
 function prompt($text)
 {
-        echo "$text\n";
+        echo "$text";
         return trim(fgets(STDIN));
 }

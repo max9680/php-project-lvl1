b2a372017ecf0a31d2c77c42cda1f9bd8e1af0e7
m.vorobyev <max9680@mail.ru>
2022-06-16 15:09:32 +0300

    Add brain-calc game

---
+++ src/Engine.php
@@ -0,0 +1,26 @@
+<?php
+namespace BrainGames\Engine;
+require_once __DIR__  . '/../vendor/autoload.php';
+
+use function BrainGames\Cli\line;
+use function BrainGames\Cli\prompt;
+
+function welcome()
+{
+    line("Welcome to the Brain Game!\n");
+    $name = prompt('May I have your name? ');
+    line("Hello, %s!\n", $name);
+    return $name;
+}
+
+function result($numberCorrectAnswers, $name, $incorrectAnswer)
+{
+        if ($incorrectAnswer == 1) {
+            line("Incorrect answer. ");
+        } 
+        if ($numberCorrectAnswers == 3) {
+            line("Congratilations, $name!\n");
+        } else {
+            line("Let's try again, $name!\n");
+}
+}

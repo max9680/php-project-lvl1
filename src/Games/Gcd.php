80a1837d7f46388434badc74327fac419537c8be
m.vorobyev <max9680@mail.ru>
2022-07-04 17:01:00 +0300

    Modify Gcd.php for function gameEngine

--- src/Games/Gcd.php
+++ src/Games/Gcd.php
@@ -3,39 +3,24 @@
 namespace BrainGames\Games\Gcd;
 
 use function BrainGames\Engine\welcome;
-use function BrainGames\Engine\result;
-use function cli\line;
-use function cli\prompt;
+use function BrainGames\Engine\gameEngine;
 
 function runGame()
 {
-    $name = welcome();
-    line("Find the greatest common divisor of given numbers.");
-    $numberCorrectAnswers = 0;
-    $incorrectAnswer = 0;
+    $name = welcome("Find the greatest common divisor of given numbers.");
     $numberGames = 3;
     for ($i = 0; $i < $numberGames; $i++) {
         $startNumber = 1;
         $endNumber = 100;
         $numberOne = rand($startNumber, $endNumber);
         $numberTwo = rand($startNumber, $endNumber);
-        line("Question: %s", "$numberOne $numberTwo");
-        $answer = prompt("Your answer");
-        if (!is_numeric($answer)) {
-            $incorrectAnswer = 1;
-            break;
-        } else {
-            $answer = intval($answer);
-        }
-
         if ($numberOne > $numberTwo) {
-                $firstNumber = $numberTwo;
-                    $secondNumber = $numberOne;
+            $firstNumber = $numberTwo;
+            $secondNumber = $numberOne;
         } else {
-                $firstNumber = $numberOne;
-                    $secondNumber = $numberTwo;
+            $firstNumber = $numberOne;
+            $secondNumber = $numberTwo;
         }
-
         $correctAnswer = $firstNumber;
         while ($correctAnswer > 0) {
             if ((($firstNumber % $correctAnswer ) == 0) && (($secondNumber % $correctAnswer ) == 0)) {
@@ -43,15 +28,8 @@ function runGame()
             }
             $correctAnswer--;
         }
-
-        if ($answer == $correctAnswer) {
-            line("Correct!");
-            $numberCorrectAnswers++;
-        } else {
-            line("%s is wrong answer ;(. ", $answer);
-            line("Correct answer was %s.", $correctAnswer);
-            break;
-        }
+        $gameData[$i][0] = "$numberOne $numberTwo";
+        $gameData[$i][1] = $correctAnswer;
     }
-    result($numberCorrectAnswers, $name, $incorrectAnswer);
+    gameEngine($gameData, $name, $numberGames);
 }

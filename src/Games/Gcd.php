e4503247f43473e02caf069005124298d6957e03
m.vorobyev <max9680@mail.ru>
2022-07-25 17:51:16 +0300

    Refactoring Gcd.php

--- src/Games/Gcd.php
+++ src/Games/Gcd.php
@@ -11,7 +11,11 @@ const GAME_DESCRIPTION = "Find the greatest common divisor of given numbers.";
 function getGreatestCommonDivider(int $number1, int $number2)
 {
     while ($number1 !== 0 && $number2 !== 0) {
-        ($number1 > $number2) ? ($number1 = $number1 % $number2) : ($number2 = $number2 % $number1);
+        if ($number1 > $number2) {
+            $number1 = $number1 % $number2;
+        } else {
+            $number2 = $number2 % $number1;
+        }
     }
     return ($number1 + $number2);
 }
